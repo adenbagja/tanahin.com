ead1452eb525dc3578826c8ba2c459cc25626a32
adenbagja <adenbagja45@gmail.com>
2018-11-14 06:35:23 +0700

    tambahan

---
+++ googlemaps/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>API MAPS</title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<script type="text/javascript" src="googlemaps.js"></script>
+	<style type="text/css">
+		.container {
+			height: 450px;
+		}
+		#map{
+			width: 100%;
+			height: 100%;
+			border: 1px solid blue;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<center><h1>Acces coogle map</h1></center>
+		<div id="map"></div>
+	</div>
+</body>
+</html>
